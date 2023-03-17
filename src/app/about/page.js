27de889941dc8103864a0bf30e2d6bcca5ec453c
Abhishek <sahniak56@gmail.com>
2023-03-18 03:07:35 +0530

    about us

---
+++ src/app/about/page.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='max-w-6xl mx-auto space-y-4 p4 px-4'>
+            <h1 className='text-2xl font-medium text-red-600 text-center'>About Us</h1>
+            <p>
+                IMDb (an acronym for Internet Movie Database)[2] is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews. IMDb began as a fan-operated movie database on the Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993. Since 1998, it is now owned and operated by IMDb.com, Inc., a subsidiary of Amazon.
+
+                As of March 2022, the database contained some 10.1 million titles (including television episodes) and 11.5 million person records.[3] Additionally, the site had 83 million registered users. The site's message boards were disabled in February 2017.
+
+                As of February 12, 2023, IMDb is the 54th most visited website in the world, according to website ranker Alexa.[4]
+
+            </p>
+            <p>
+
+                The title and talent pages of IMDb are accessible to all users, but only registered and logged-in users can submit new material and suggest edits to existing entries. Most of the site's data has been provided by these volunteers. Registered users with a proven track record are able to add and make corrections to cast lists, credits, and some other data points. However, the addition and removal of images, and alterations to titles, cast and crew names, character names, and plot summaries are subject to an approval process; this usually takes between 24 and 72 hours.
+
+                On October 2, 2007,[citation needed] character filmographies were added. Character entries are created from character listings in the main filmography database, and as such do not need any additional verification by IMDb staff. They have already been verified when they are added to the main filmography.
+
+                Registered users can choose their username, and most are pseudonymous. There is no single index of contributors, no index on each profile page of the items contributed, and—except for plot synopses and biographies—no identification of contributors to each product's or person's data pages. Users are also invited to rate titles on a scale of 1 to 10, and the totals are converted into a weighted mean-rating, with filters in place to mitigate ballot-stuffing.
+
+                User profile pages show a user's registration date and, optionally, their personal ratings of titles. Since 2015, "badges" can be added showing a count of contributions. These badges range from total contributions made to independent categories such as photos, trivia, and biographies. If a registered user or visitor is in the entertainment industry and has an IMDb page, they can add photos through IMDbPRO.
+            </p>
+        </div>
+    )
+}
+
+export default page
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,20 @@ import MenuItem from './MenuItem'
 import {AiFillHome,AiFillInfoCircle} from 'react-icons/ai'
 import Link from 'next/link'
 import DarkMode from './DarkMode'
-import Providers from '@/app/Providers'
 
 const Header = () => {
     return (
         <>
-        {/* <Providers> */}
         <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
             <div className='flex'>
                 <MenuItem title={"HOME"} address={"/"} Icon={AiFillHome}/>
-                <MenuItem title={"About"} address={"/about"} Icon={AiFillInfoCircle}/>
+                <MenuItem title={"ABOUT"} address={"/about"} Icon={AiFillInfoCircle}/>
             </div>
             <div className='flex items-center space-x-6 mx-3 '>
                 <DarkMode/>
                <Link href={'/'}><h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span></h2></Link>
             </div>
             </div>
-        {/* </Providers> */}
         </>
     )
 }
